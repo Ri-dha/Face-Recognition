d148b87b14a5f2c063c560dc3843d215cc897169
ridha-azu-employee <152700871+ridha-azu-employee@users.noreply.github.com>
2024-01-31 18:32:57 +0300

    added postgres

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'face_recognition',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
